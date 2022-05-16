0d8a7e456dcbb0e4ab2e2f8bc7e53d09fa76ba0d
Liang Geng <geng.161@osu.edu>
2022-05-16 12:37:13 -0400

    Multiple writer

--- tools/perf/bench/epoll-wait.c
+++ tools/perf/bench/epoll-wait.c
@@ -84,11 +84,17 @@
 
 #include <err.h>
 
-#define printinfo(fmt, arg...) \
-	do { if (__verbose) { printf(fmt, ## arg); fflush(stdout); } } while (0)
+#define printinfo(fmt, arg...)                                                 \
+	do {                                                                   \
+		if (__verbose) {                                               \
+			printf(fmt, ##arg);                                    \
+			fflush(stdout);                                        \
+		}                                                              \
+	} while (0)
 
 static unsigned int nthreads = 0;
-static unsigned int nsecs    = 8;
+static unsigned int nwthreads = 1;
+static unsigned int nsecs = 8;
 static bool wdone, done, __verbose, randomize, nonblocking;
 
 /*
@@ -125,31 +131,44 @@ struct worker {
 	int *fdmap;
 };
 
+struct writer {
+	int tid;
+	pthread_t thread;
+	struct worker *worker;
+};
+
 static const struct option options[] = {
 	/* general benchmark options */
 	OPT_UINTEGER('t', "threads", &nthreads, "Specify amount of threads"),
+	OPT_UINTEGER('w', "wthreads", &nwthreads, "Specify amount of threads"),
 	OPT_UINTEGER('r', "runtime", &nsecs, "Specify runtime (in seconds)"),
-	OPT_UINTEGER('f', "nfds",    &nfds,  "Specify amount of file descriptors to monitor for each thread"),
-    	OPT_UINTEGER('s', "sleep", &sleep_nano, "Specify sleep time in nano for wrting thread"),
-	OPT_BOOLEAN( 'n', "noaffinity",  &noaffinity,   "Disables CPU affinity"),
-	OPT_BOOLEAN('R', "randomize", &randomize,   "Enable random write behaviour (default is lineal)"),
-	OPT_BOOLEAN( 'v', "verbose", &__verbose, "Verbose mode"),
+	OPT_UINTEGER(
+		'f', "nfds", &nfds,
+		"Specify amount of file descriptors to monitor for each thread"),
+	OPT_UINTEGER('s', "sleep", &sleep_nano,
+		     "Specify sleep time in nano for wrting thread"),
+	OPT_BOOLEAN('n', "noaffinity", &noaffinity, "Disables CPU affinity"),
+	OPT_BOOLEAN('R', "randomize", &randomize,
+		    "Enable random write behaviour (default is lineal)"),
+	OPT_BOOLEAN('v', "verbose", &__verbose, "Verbose mode"),
 
 	/* epoll specific options */
-	OPT_BOOLEAN( 'm', "multiq",  &multiq,   "Use multiple epoll instances (one per thread)"),
-	OPT_BOOLEAN( 'B', "nonblocking", &nonblocking, "Nonblocking epoll_wait(2) behaviour"),
-	OPT_UINTEGER( 'N', "nested",  &nested,   "Nesting level epoll hierarchy (default is 0, no nesting)"),
-	OPT_BOOLEAN( 'S', "oneshot",  &oneshot,   "Use EPOLLONESHOT semantics"),
-	OPT_BOOLEAN( 'E', "edge",  &et,   "Use Edge-triggered interface (default is LT)"),
+	OPT_BOOLEAN('m', "multiq", &multiq,
+		    "Use multiple epoll instances (one per thread)"),
+	OPT_BOOLEAN('B', "nonblocking", &nonblocking,
+		    "Nonblocking epoll_wait(2) behaviour"),
+	OPT_UINTEGER('N', "nested", &nested,
+		     "Nesting level epoll hierarchy (default is 0, no nesting)"),
+	OPT_BOOLEAN('S', "oneshot", &oneshot, "Use EPOLLONESHOT semantics"),
+	OPT_BOOLEAN('E', "edge", &et,
+		    "Use Edge-triggered interface (default is LT)"),
 
 	OPT_END()
 };
 
-static const char * const bench_epoll_wait_usage[] = {
-	"perf bench epoll wait <options>",
-	NULL
+static const char *const bench_epoll_wait_usage[] = {
+	"perf bench epoll wait <options>", NULL
 };
-
 
 /*
  * Arrange the N elements of ARRAY in random order.
@@ -171,26 +190,25 @@ static void shuffle(void *array, size_t n, size_t size)
 		err(EXIT_FAILURE, "calloc");
 
 	for (i = 1; i < n; ++i) {
-		size_t j =   i + rand() / (RAND_MAX / (n - i) + 1);
+		size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
 		j *= size;
 
 		memcpy(aux, &carray[j], size);
-		memcpy(&carray[j], &carray[i*size], size);
-		memcpy(&carray[i*size], aux, size);
+		memcpy(&carray[j], &carray[i * size], size);
+		memcpy(&carray[i * size], aux, size);
 	}
 
 	free(aux);
 }
 
-
 static void *workerfn(void *arg)
 {
 	int fd, ret, r;
-	struct worker *w = (struct worker *) arg;
+	struct worker *w = (struct worker *)arg;
 	unsigned long ops = w->ops;
 	struct epoll_event ev;
 	uint64_t val;
-	int to = nonblocking? 0 : -1;
+	int to = nonblocking ? 0 : -1;
 	int efd = multiq ? w->epollfd : epollfd;
 
 	pthread_mutex_lock(&thread_lock);
@@ -202,11 +220,11 @@ static void *workerfn(void *arg)
 
 	do {
 		/*
-		 * Block indefinitely waiting for the IN event.
-		 * In order to stress the epoll_wait(2) syscall,
-		 * call it event per event, instead of a larger
-		 * batch (max)limit.
-		 */
+         * Block indefinitely waiting for the IN event.
+         * In order to stress the epoll_wait(2) syscall,
+         * call it event per event, instead of a larger
+         * batch (max)limit.
+         */
 		do {
 			ret = epoll_wait(efd, &ev, 1, to);
 		} while ((ret < 0 && errno == EINTR) || ret == 0);
@@ -231,7 +249,7 @@ static void *workerfn(void *arg)
 		}
 
 		ops++;
-	}  while (!done);
+	} while (!done);
 
 	if (multiq)
 		close(w->epollfd);
@@ -262,8 +280,8 @@ static void nest_epollfd(struct worker *w)
 	ev.data.u64 = i; /* any number */
 
 	for (i = nested - 1; i; i--) {
-		if (epoll_ctl(epollfdp[i - 1], EPOLL_CTL_ADD,
-			      epollfdp[i], &ev) < 0)
+		if (epoll_ctl(epollfdp[i - 1], EPOLL_CTL_ADD, epollfdp[i],
+			      &ev) < 0)
 			err(EXIT_FAILURE, "epoll_ctl");
 	}
 
@@ -271,8 +289,7 @@ static void nest_epollfd(struct worker *w)
 		err(EXIT_FAILURE, "epoll_ctl");
 }
 
-static void toggle_done(int sig __maybe_unused,
-			siginfo_t *info __maybe_unused,
+static void toggle_done(int sig __maybe_unused, siginfo_t *info __maybe_unused,
 			void *uc __maybe_unused)
 {
 	/* inform all threads that we're done for the day */
@@ -287,8 +304,7 @@ static void print_summary(void)
 	double stddev = stddev_stats(&throughput_stats);
 
 	printf("\nAveraged %ld operations/sec (+- %.2f%%), total secs = %d\n",
-	       avg, rel_stddev_stats(stddev, avg),
-	       (int)bench__runtime.tv_sec);
+	       avg, rel_stddev_stats(stddev, avg), (int)bench__runtime.tv_sec);
 }
 
 static int do_threads(struct worker *worker, struct perf_cpu_map *cpu)
@@ -306,8 +322,8 @@ static int do_threads(struct worker *worker, struct perf_cpu_map *cpu)
 		events |= EPOLLET;
 
 	printinfo("starting worker/consumer %sthreads%s\n",
-		  noaffinity ?  "":"CPU affinity ",
-		  nonblocking ? " (nonblocking)":"");
+		  noaffinity ? "" : "CPU affinity ",
+		  nonblocking ? " (nonblocking)" : "");
 	if (!noaffinity)
 		pthread_attr_init(&thread_attr);
 
@@ -344,28 +360,31 @@ static int do_threads(struct worker *worker, struct perf_cpu_map *cpu)
 			ev.data.fd = w->fdmap[j];
 			ev.events = events;
 
-			ret = epoll_ctl(efd, EPOLL_CTL_ADD,
-					w->fdmap[j], &ev);
+			ret = epoll_ctl(efd, EPOLL_CTL_ADD, w->fdmap[j], &ev);
 			if (ret < 0)
 				err(EXIT_FAILURE, "epoll_ctl");
 		}
 
 		if (!noaffinity) {
 			CPU_ZERO_S(size, cpuset);
-			CPU_SET_S(perf_cpu_map__cpu(cpu, i % perf_cpu_map__nr(cpu)).cpu,
-					size, cpuset);
+			CPU_SET_S(perf_cpu_map__cpu(cpu,
+						    i % perf_cpu_map__nr(cpu))
+					  .cpu,
+				  size, cpuset);
 
-			ret = pthread_attr_setaffinity_np(&thread_attr, size, cpuset);
+			ret = pthread_attr_setaffinity_np(&thread_attr, size,
+							  cpuset);
 			if (ret) {
 				CPU_FREE(cpuset);
-				err(EXIT_FAILURE, "pthread_attr_setaffinity_np");
+				err(EXIT_FAILURE,
+				    "pthread_attr_setaffinity_np");
 			}
 
 			attrp = &thread_attr;
 		}
 
 		ret = pthread_create(&w->thread, attrp, workerfn,
-				     (void *)(struct worker *) w);
+				     (void *)(struct worker *)w);
 		if (ret) {
 			CPU_FREE(cpuset);
 			err(EXIT_FAILURE, "pthread_create");
@@ -381,23 +400,34 @@ static int do_threads(struct worker *worker, struct perf_cpu_map *cpu)
 
 static void *writerfn(void *p)
 {
-	struct worker *worker = p;
+	struct writer *writer = p;
+	struct worker *r_worker = writer->worker;
 	size_t i, j, iter;
+	size_t avg_nthreads, begin_tid, end_tid;
 	const uint64_t val = 1;
 	ssize_t sz;
-	struct timespec ts = { .tv_sec = 0,
-			       .tv_nsec = sleep_nano };
+	struct timespec ts = { .tv_sec = 0, .tv_nsec = sleep_nano };
 
-	printinfo("starting writer-thread: doing %s writes ...\n",
-		  randomize? "random":"lineal");
+	avg_nthreads = (nthreads + nwthreads - 1) / nwthreads;
+	begin_tid = min_t(size_t, avg_nthreads * writer->tid, nthreads);
+	end_tid = min_t(size_t, avg_nthreads * (writer->tid + 1), nthreads);
+
+	if (begin_tid == end_tid) {
+		err(EXIT_FAILURE, "Too many writers");
+	}
+
+	printinfo(
+		"starting writer-thread %d writing for (%zu - %zu): doing %s writes ...\n",
+		writer->tid, begin_tid, end_tid,
+		randomize ? "random" : "lineal");
 
 	for (iter = 0; !wdone; iter++) {
 		if (randomize) {
-			shuffle((void *)worker, nthreads, sizeof(*worker));
+			shuffle((void *)r_worker, nthreads, sizeof(*r_worker));
 		}
 
-		for (i = 0; i < nthreads; i++) {
-			struct worker *w = &worker[i];
+		for (i = begin_tid; i < end_tid; i++) {
+			struct worker *w = &r_worker[i];
 
 			if (randomize) {
 				shuffle((void *)w->fdmap, nfds, sizeof(int));
@@ -405,24 +435,84 @@ static void *writerfn(void *p)
 
 			for (j = 0; j < nfds; j++) {
 				do {
-					sz = write(w->fdmap[j], &val, sizeof(val));
+					sz = write(w->fdmap[j], &val,
+						   sizeof(val));
 				} while (!wdone && (sz < 0 && errno == EAGAIN));
 			}
 		}
 		if (sleep_nano > 0) {
-		        nanosleep(&ts, NULL);
+			nanosleep(&ts, NULL);
 		}
 	}
 
-	printinfo("exiting writer-thread (total full-loops: %zd)\n", iter);
+	printinfo("exiting writer-thread %d (total full-loops: %zd)\n",
+		  writer->tid, iter);
 	return NULL;
+}
+
+static int do_write_threads(struct writer *w_writer, struct worker *r_worker,
+			    struct perf_cpu_map *cpu)
+{
+	pthread_attr_t thread_attr, *attrp = NULL;
+	cpu_set_t *cpuset;
+	size_t size;
+	int ret = 0;
+	int nrcpus;
+	size_t i;
+
+	printinfo("starting worker/producer %sthreads\n",
+		  noaffinity ? "" : "CPU affinity ");
+	if (!noaffinity)
+		pthread_attr_init(&thread_attr);
+
+	nrcpus = perf_cpu_map__nr(cpu);
+	cpuset = CPU_ALLOC(nrcpus);
+	BUG_ON(!cpuset);
+	size = CPU_ALLOC_SIZE(nrcpus);
+
+	for (i = 0; i < nwthreads; i++) {
+		struct writer *w = &w_writer[i];
+
+		w->tid = i;
+		w->worker = calloc(nthreads, sizeof(struct worker));
+		memcpy(w->worker, r_worker, sizeof(struct worker) * nthreads);
+
+		if (!noaffinity) {
+			CPU_ZERO_S(size, cpuset);
+			CPU_SET_S(perf_cpu_map__cpu(cpu,
+						    i % perf_cpu_map__nr(cpu))
+					  .cpu,
+				  size, cpuset);
+
+			ret = pthread_attr_setaffinity_np(&thread_attr, size,
+							  cpuset);
+			if (ret) {
+				CPU_FREE(cpuset);
+				err(EXIT_FAILURE,
+				    "pthread_attr_setaffinity_np");
+			}
+
+			attrp = &thread_attr;
+		}
+		ret = pthread_create(&w->thread, attrp, writerfn,
+				     (void *)(struct worker *)w);
+		if (ret) {
+			CPU_FREE(cpuset);
+			err(EXIT_FAILURE, "pthread_create");
+		}
+	}
+
+	CPU_FREE(cpuset);
+	if (!noaffinity)
+		pthread_attr_destroy(&thread_attr);
+
+	return ret;
 }
 
 static int cmpworker(const void *p1, const void *p2)
 {
-
-	struct worker *w1 = (struct worker *) p1;
-	struct worker *w2 = (struct worker *) p2;
+	struct worker *w1 = (struct worker *)p1;
+	struct worker *w2 = (struct worker *)p2;
 	return w1->tid > w2->tid;
 }
 
@@ -431,9 +521,9 @@ int bench_epoll_wait(int argc, const char **argv)
 	int ret = 0;
 	struct sigaction act;
 	unsigned int i;
-	struct worker *worker = NULL;
+	struct worker *r_worker = NULL;
+	struct writer *w_writer = NULL;
 	struct perf_cpu_map *cpu;
-	pthread_t wthread;
 	struct rlimit rl, prevrl;
 
 	argc = parse_options(argc, argv, options, bench_epoll_wait_usage, 0);
@@ -458,8 +548,8 @@ int bench_epoll_wait(int argc, const char **argv)
 			err(EXIT_FAILURE, "epoll_create");
 
 		/*
-		 * Deal with nested epolls, if any.
-		 */
+         * Deal with nested epolls, if any.
+         */
 		if (nested)
 			nest_epollfd(NULL);
 	}
@@ -471,22 +561,25 @@ int bench_epoll_wait(int argc, const char **argv)
 	if (!nthreads)
 		nthreads = perf_cpu_map__nr(cpu) - 1;
 
-	worker = calloc(nthreads, sizeof(*worker));
-	if (!worker) {
+	r_worker = calloc(nthreads, sizeof(struct worker));
+	w_writer = calloc(nwthreads, sizeof(struct writer));
+	if (!r_worker || !w_writer) {
 		goto errmem;
 	}
 
 	if (getrlimit(RLIMIT_NOFILE, &prevrl))
 		err(EXIT_FAILURE, "getrlimit");
 	rl.rlim_cur = rl.rlim_max = nfds * nthreads * 2 + 50;
-	printinfo("Setting RLIMIT_NOFILE rlimit from %" PRIu64 " to: %" PRIu64 "\n",
+	printinfo("Setting RLIMIT_NOFILE rlimit from %" PRIu64 " to: %" PRIu64
+		  "\n",
 		  (uint64_t)prevrl.rlim_max, (uint64_t)rl.rlim_max);
 	if (setrlimit(RLIMIT_NOFILE, &rl) < 0)
 		err(EXIT_FAILURE, "setrlimit");
 
 	printf("Run summary [PID %d]: %d threads monitoring%s on "
 	       "%d file-descriptors for %d secs.\n\n",
-	       getpid(), nthreads, oneshot ? " (EPOLLONESHOT semantics)": "", nfds, nsecs);
+	       getpid(), nthreads, oneshot ? " (EPOLLONESHOT semantics)" : "",
+	       nfds, nsecs);
 
 	init_stats(&throughput_stats);
 	pthread_mutex_init(&thread_lock, NULL);
@@ -495,34 +588,33 @@ int bench_epoll_wait(int argc, const char **argv)
 
 	threads_starting = nthreads;
 
-	do_threads(worker, cpu);
+	do_threads(r_worker, cpu);
 
 	pthread_mutex_lock(&thread_lock);
 	while (threads_starting)
 		pthread_cond_wait(&thread_parent, &thread_lock);
 	pthread_cond_broadcast(&thread_worker);
-    gettimeofday(&bench__start, NULL);
+	gettimeofday(&bench__start, NULL);
 	pthread_mutex_unlock(&thread_lock);
 
 	/*
-	 * At this point the workers should be blocked waiting for read events
-	 * to become ready. Launch the writer which will constantly be writing
-	 * to each thread's fdmap.
-	 */
-	ret = pthread_create(&wthread, NULL, writerfn,
-			     (void *)(struct worker *) worker);
-	if (ret)
-		err(EXIT_FAILURE, "pthread_create");
+	* At this point the workers should be blocked waiting for read events
+	* to become ready. Launch the writer which will constantly be writing
+	* to each thread's fdmap.
+	*/
+	do_write_threads(w_writer, r_worker, cpu);
 
 	sleep(nsecs);
 	toggle_done(0, NULL, NULL);
 	printinfo("main thread: toggling done\n");
 
-	sleep(1); /* meh */
 	wdone = true;
-	ret = pthread_join(wthread, NULL);
-	if (ret)
-		err(EXIT_FAILURE, "pthread_join");
+
+	for (i = 0; i < nwthreads; i++) {
+		ret = pthread_join(w_writer[i].thread, NULL);
+		if (ret)
+			err(EXIT_FAILURE, "pthread_join");
+	}
 
 	/* cleanup & report results */
 	pthread_cond_destroy(&thread_parent);
@@ -531,21 +623,23 @@ int bench_epoll_wait(int argc, const char **argv)
 
 	/* sort the array back before reporting */
 	if (randomize)
-		qsort(worker, nthreads, sizeof(struct worker), cmpworker);
+		qsort(r_worker, nthreads, sizeof(struct worker), cmpworker);
 
 	for (i = 0; i < nthreads; i++) {
-		unsigned long t = bench__runtime.tv_sec > 0 ?
-			worker[i].ops / bench__runtime.tv_sec : 0;
+		unsigned long t =
+			bench__runtime.tv_sec > 0 ?
+				r_worker[i].ops / bench__runtime.tv_sec :
+				0;
 
 		update_stats(&throughput_stats, t);
 
 		if (nfds == 1)
 			printf("[thread %2d] fdmap: %p [ %04ld ops/sec ]\n",
-			       worker[i].tid, &worker[i].fdmap[0], t);
+			       r_worker[i].tid, &r_worker[i].fdmap[0], t);
 		else
 			printf("[thread %2d] fdmap: %p ... %p [ %04ld ops/sec ]\n",
-			       worker[i].tid, &worker[i].fdmap[0],
-			       &worker[i].fdmap[nfds-1], t);
+			       r_worker[i].tid, &r_worker[i].fdmap[0],
+			       &r_worker[i].fdmap[nfds - 1], t);
 	}
 
 	print_summary();
